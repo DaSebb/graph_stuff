eb75db76bf9a9d06ddc3a0374fdfaa57b1c85627
Sebastian <sebastian@starfish.team>
2024-04-02 23:47:17 +0200

    removed default header of Phoenix in app layout

--- lib/graph_stuff_web/live/decrypt_form_live.ex
+++ lib/graph_stuff_web/live/decrypt_form_live.ex
@@ -1,17 +1,9 @@
 defmodule GraphStuffWeb.DecryptFormLive do
   use GraphStuffWeb, :live_view
 
-  # def render(assigns) do
-  #   ~H"""
-  #   <div>
-  #   Hello Test
-  #   </div>
-  #   """
-  # end
-
   def render(assigns) do
     ~H"""
-    <.simple_form id="decrypt_form" for={@form} phx_change="validate" phx-submit="save">
+    <.simple_form id="decrypt_form" for={@form} phx-change="validate" phx-submit="save">
       <.input field={@form[:jwe_input]} type="textarea" label="Input JWE to decrypt" />
       <:actions>
         <.button>Submit</.button>
@@ -19,21 +11,6 @@ defmodule GraphStuffWeb.DecryptFormLive do
     </.simple_form>
     """
   end
-
-  # def render(assigns) do
-  #   ~H"""
-  #   <div class="mx-auto max-w-sm">
-  #     <.header class="text-center">
-  #       Decrypt a JWE token
-  #     </.header>
-
-  #     <.form for={@form} phx-update="ignore">
-  #       <.input field={@form[:textarea]} type="textarea" label="JWE" required />
-  #       <.button>Save</.button>
-  #     </.form>
-  #   </div>
-  #   """
-  # end
 
   def mount(_params, _session, socket) do
     form_fields = %{"jwe_input" => ""}
